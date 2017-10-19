f2e953aac68ce4a26d1fde58be4c1532f2cb8881
Chent <bz20100405@163.com>
2017-10-19 21:51:19 +0800

    finish v1.0 !!!

--- 1.js
+++ 1.js
@@ -1,22 +1,97 @@
+/* 
+姓名: 陈涛
+学号: 16340031
+组别：晚上班23组
+内容：1. 作业的基本要求
+      2. 支持键盘输入(右边小键盘，不支持括号, CE对应Delete键)
+      3. 禁止括号以外的运算符的连续使用
+      4. 小数点前自动补0(.2 -> 0.2)
+*/
 var str = "0";
 
 window.onload = function() {
 	document.getElementById("result").value = str;
+	var x = document.getElementsByClassName("number");
+	var y = document.getElementsByClassName("operator");
+	for (var i = 0; i < x.length; i++)
+		x[i].onclick = function(){fun_number(this.childNodes[0].textContent);};  //this指调用函数的那个对象
+	for (var i = 0; i < y.length; i++)
+		y[i].onclick = function(){fun_operator(this.childNodes[0].textContent);};  //这里是回调函数，方便后面使用，所以并不能用x[i]或者y[i]
+	document.getElementById("delete").onclick = fun_delete;
+	document.getElementById("equal").onclick = fun_equal;
+	document.getElementById("clear").onclick = fun_clear; 
 }
 
-function fun(a) {
-	str+=document.getElementById(a).innerHTML;
+function fun_number(a) {
+	if(document.getElementById("result").value == "0")
+		str = document.getElementById(a).innerHTML;
+	else
+		str+=document.getElementById(a).innerHTML;
 	document.getElementById("result").value = str;
 }
 
-function fun1() {
-	var answer = eval(str);
-	document.getElementById("result").value = answer;
-	str=answer;
+function fun_delete() {
+	str = str.substring(0, str.length-1);
+	if(str.length == 0) {
+		str = '0';
+		document.getElementById("result").value = str;
+	}
+	else
+		document.getElementById("result").value = str;	
 }
 
-function fun2() {
+function fun_operator(a) {
+	if(str == '0' && (a == "("||a == ")"))
+		str = document.getElementById(a).innerHTML;
+	else{
+		if(a == "." && (str[str.length-1] < '0' || str[str.length-1] > '9') && str[str.length-1] != '.')
+			str+="0.";                                                            //小数点前面补0
+		else
+			if(str[str.length-1] != a || a == '(' || a == ')')
+				str+=document.getElementById(a).innerHTML;                        //不允许运算符重复（除了括号）
+	}
+	document.getElementById("result").value = str;	
+}
+
+function fun_equal() {
+	try {
+		var answer = eval(str).toString();
+	}
+	catch(err) {
+		alert("非法输入!");
+		document.getElementById("result").value = '0';
+		str = '0';
+		return;
+	}
+	document.getElementById("result").value = answer;
+	str=answer; //连续计算
+}
+
+function fun_clear() {
 	document.getElementById("result").value = "";
 	str="0";
 	document.getElementById("result").value = str;
 }
+
+document.onkeydown=function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+		if(e && e.keyCode >= 96 && e.keyCode <= 105){
+	 		fun_number((e.keyCode-96).toString());
+	    }
+	    if(e && e.keyCode == 8)
+	    	fun_delete();
+	    if(e && e.keyCode == 13)
+	    	fun_equal();
+	    if(e && e.keyCode == 46)
+	    	fun_clear();
+	    if(e && e.keyCode == 106)
+	    	fun_operator("*");
+	    if(e && e.keyCode == 107)
+	    	fun_operator("+");
+	    if(e && e.keyCode == 109)
+	    	fun_operator("-");
+	    if(e && e.keyCode == 110)
+	    	fun_operator(".");
+	    if(e && e.keyCode == 111)
+	    	fun_operator("/");
+};
